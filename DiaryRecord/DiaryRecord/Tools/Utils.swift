ffc44b891d6026dcfe4ce41cf8a99c28e3f71133
WangTao <878273817@qq.com>
2017-10-30 17:51:26 +0800

    修改首页StoryBoard布局

--- DiaryRecord/DiaryRecord/Tools/Utils.swift
+++ DiaryRecord/DiaryRecord/Tools/Utils.swift
@@ -12,17 +12,18 @@ class Utils: NSObject {
 
     ///保存用户信息
     static func savaUserInfo(dic : Dictionary<String,String>){
+        
         USERDEFAUTS.set(dic, forKey: USERDEFAUTS_KEY_USERMODEL)
         USERDEFAUTS.synchronize()
+        
     }
     
     ///获取保存的用户信息
     static func getUserInfo() -> UserModel? {
+        
         let resultDic : Dictionary<String,String>? = USERDEFAUTS.object(forKey: USERDEFAUTS_KEY_USERMODEL) as? Dictionary<String, String>
-//        if(resultDic?.count <= 0 ){
-//            return nil
-//        }
         return UserModel.init(dic: resultDic)
+        
     }
     
     
